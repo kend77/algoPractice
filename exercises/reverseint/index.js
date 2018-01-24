dad55b443172885242719476e215beecbc9ec689
Kenny Diaz <kend77@gmail.com>
2018-01-23 21:37:15 -0500

    add reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+	let stringed = n + ''
+	let negative = false;
+	let reversed = ''
+	if(stringed[0] === '-'){
+		stringed = stringed.slice(1)
+		negative = true
+	}	
+	for(let i = 0; i < stringed.length; i++){
+		reversed = stringed[i] + reversed	
+	}
+	return negative ? +('-' + reversed) : +reversed
+}
 
 module.exports = reverseInt;
