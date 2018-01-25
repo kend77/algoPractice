b0840cd3a79074c694217d67fc735e31ca4ffadb
Kenny Diaz <kend77@gmail.com>
2018-01-24 21:08:21 -0500

    add fizzbuzz, maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	let seen = {}
+	let max = null
+	let maxCount = 0;
+	for(let i = 0; i < str.length; i++){
+		seen[str[i]] ? seen[str[i]]++ : seen[str[i]] = 1
+	}
+	for(let char in seen) {
+		if(seen[char] > maxCount) {
+			maxCount = seen[char]
+			max = char
+		}
+	}
+	return max
+}
 
 module.exports = maxChar;
