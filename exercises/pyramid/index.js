5cd284a21297aa502cc216461115434d8cd14336
Kenny Diaz <kend77@gmail.com>
2018-01-30 15:11:19 -0500

    add pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,20 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let middle = n - 1
+  for(let i = 0; i < n; i++) {
+    let level = ''
+    for(let j = 0; j < (n * 2 - 1); j++) {
+      if(j >= middle - i && j <= middle + i) {
+        level += '#'
+      } else {
+        level += ' '
+      }
+    }
+    console.log(level)
+  }
+}
+
 
 module.exports = pyramid;
