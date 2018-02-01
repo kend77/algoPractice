c2d9bb1fd483c626d889bc9560b95b77aafc691c
Kenny Diaz <kend77@gmail.com>
2018-02-01 14:30:03 -0500

    add queue, weave

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,16 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = []
+  }
+  add(value) {
+    this.data.push(value)
+  }
+  remove() {
+    return this.data.shift()
+  }
+}
 
 module.exports = Queue;
