8ca084225c8bcc7b0b0a3a02cd8658af67f5dc4e
Kenny Diaz <kend77@gmail.com>
2018-02-15 18:27:05 -0500

    add level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let queue = [root, 's']
+  let counters = [0]
+
+  while(queue.length > 1) {
+    let current = queue.shift()
+
+    if(current === 's') {
+      counters.push(0)
+      queue.push('s')
+    } else {
+      queue.push(...current.children)
+      counters[counters.length - 1]++
+    }
+  }
+  return counters
+}
+
+
 
 module.exports = levelWidth;
